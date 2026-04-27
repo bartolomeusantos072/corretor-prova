f94e330d3c0173f1981fd471db34b7324e572c41
Bartolomeu Uender <bartolomeusantos072@gmail.com>
2026-04-27 16:28:16 -0300

    botao remover

--- script.js
+++ script.js
@@ -31,8 +31,9 @@ const criarLinhaQuestao = (numero, dados = null) => {
         <input type="number" step="0.1" class="input-valor border w-16 p-1 rounded ml-auto text-center text-sm focus:ring-1 focus:ring-green-500 outline-none" 
                value="${valorPadrao}">
         
-        <button type="button" class="btn-remover-linha text-red-400 hover:text-red-600 ml-2 opacity-0 group-hover:opacity-100 transition-all" title="Remover esta questão">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+            // Substitua o botão antigo por este no seu script.js
+        <button type="button" class="btn-remover-linha text-lg text-red-400 hover:text-red-600 ml-2 opacity-0 group-hover:opacity-100 transition-all" title="Remover esta questão">
+            🗑️
         </button>
     `;
 
